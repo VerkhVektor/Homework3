0149b2eda1cd8e671450a62e5093cea0b9edd97a
Dmitriy <dimonglin@gmail.com>
2025-12-18 22:04:25 +0500

    Homework 3 correction commit

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,22 @@ public class Main {
 
         //Первое задание
         int intVariable = 23800;
-         System.out.println("Значение переменной с типом int равно " + intVariable);
-         byte byteVariable = 120;
-         System.out.println("Значение переменной с типом byte равно " + byteVariable);
-         short shortVariable = 32766;
-         System.out.println("Значение переменной с типом short равно " + shortVariable);
-         long longVariable = 3000000;
-         System.out.println("Значение переменной с типом long равно " + longVariable);
-         float floatVariable = 2.0123f;
-         System.out.println("Значение переменной с типом float равно " + floatVariable);
-         double doubleVariable = 4.0123456;
-         System.out.println("Значение переменной с типом double равно " + doubleVariable);
+        System.out.println("Значение переменной с типом int равно " + intVariable);
+        byte byteVariable = 120;
+        System.out.println("Значение переменной с типом byte равно " + byteVariable);
+        short shortVariable = 32766;
+        System.out.println("Значение переменной с типом short равно " + shortVariable);
+        long longVariable = 3000000L;
+        System.out.println("Значение переменной с типом long равно " + longVariable);
+        float floatVariable = 2.0123f;
+        System.out.println("Значение переменной с типом float равно " + floatVariable);
+        double doubleVariable = 4.0123456;
+        System.out.println("Значение переменной с типом double равно " + doubleVariable);
 
-         // Второе задание
+        // Второе задание
 
         float a = 27.12f;
-        long b = 987678965549l;
+        long b = 987678965549L;
         double c = 2.786;
         short d = 569;
         short e = -159;
@@ -51,8 +51,8 @@ public class Main {
         byte redForClass = 2;
         byte brownForClass = 4;
         byte totalCan = 120;
-        byte cansForClass = (byte)(redForClass + brownForClass);
-        byte totalClasses = (byte)(totalCan / cansForClass);
+        byte cansForClass = (byte) (redForClass + brownForClass);
+        byte totalClasses = (byte) (totalCan / cansForClass);
         byte white = (byte) (totalClasses * 2);
         byte brown = (byte) (totalClasses * 4);
         System.out.println("В школе, где " + totalClasses + " классов, нужно " + white + " банок белой краски и " + brown + " банок коричневой краски».");
@@ -61,7 +61,7 @@ public class Main {
 
         byte bananaQuantity = 5;
         byte bananaWeight = 80;
-        short bananaTotal = (short)(bananaQuantity * bananaWeight);
+        short bananaTotal = (short) (bananaQuantity * bananaWeight);
         short milkVolume = 200;
         float convertMilkToWeight = 105f / 100f;
         float milkWeight = milkVolume * convertMilkToWeight;
@@ -70,7 +70,7 @@ public class Main {
         short iceCreamTotal = (short) (iceCreamQuantity * iceCreamWeight);
         byte eggQuantity = 4;
         byte eggWeight = 70;
-        short eggTotal = (short)(eggQuantity * eggWeight);
+        short eggTotal = (short) (eggQuantity * eggWeight);
         float totalWeight = (bananaTotal + milkWeight + iceCreamTotal + eggTotal);
         System.out.println("Вес завтрака в граммах " + totalWeight);
         float totalWeightKilo = totalWeight / 1000f;
@@ -79,15 +79,15 @@ public class Main {
         //Task 7
 
         byte goalLoose = 7;
-        short goalLooseGram = (short)(goalLoose * 1000);
+        short goalLooseGram = (short) (goalLoose * 1000);
         short minLooseByDay = 250;
         short maxLooseByDay = 500;
-        short avgLooseByDay = (short)((minLooseByDay + maxLooseByDay) / 2);
-        float minDays = (float)(goalLooseGram / minLooseByDay);
+        short avgLooseByDay = (short) ((minLooseByDay + maxLooseByDay) / 2);
+        float minDays = (float) (goalLooseGram / minLooseByDay);
         System.out.println("Минимальное количество дней для требуемого результата " + minDays);
-        float maxDays = (float)(goalLooseGram / maxLooseByDay);
+        float maxDays = (float) (goalLooseGram / maxLooseByDay);
         System.out.println("максимальное количество дней для требуемого результата " + maxDays);
-        double avgDays = (float)goalLooseGram / avgLooseByDay;
+        double avgDays = (float) goalLooseGram / avgLooseByDay;
         System.out.println("Среднее количество дней для требуемого результата " + avgDays);
 
 
@@ -95,43 +95,22 @@ public class Main {
 
         int mashaSalary = 67760;
         int denisSalary = 83690;
-        int ChristinaSalary = 76230;
-        float newMashaSalary = (float)mashaSalary * 1.1f;
-        float newDenisSalary = (float)denisSalary * 1.1f;
-        float newChristinaSalary = (float)ChristinaSalary * 1.1f;
-        float MashaOldYear = mashaSalary * 12;
-        float DenisOldYear = denisSalary * 12;
-        float ChristinaOldYear = ChristinaSalary * 12;
-        float MashaNewYear = newMashaSalary * 12;
-        float DenisNewYear = newDenisSalary * 12;
-        float ChristinaNewYear = newChristinaSalary * 12;
-        float MashaProfit = MashaNewYear - MashaOldYear;
+        int christinaSalary = 76230;
+        float newMashaSalary = (float) mashaSalary * 1.1f;
+        float newDenisSalary = (float) denisSalary * 1.1f;
+        float newChristinaSalary = (float) christinaSalary * 1.1f;
+        float mashaOldYear = mashaSalary * 12;
+        float denisOldYear = denisSalary * 12;
+        float christinaOldYear = christinaSalary * 12;
+        float mashaNewYear = newMashaSalary * 12;
+        float denisNewYear = newDenisSalary * 12;
+        float christinaNewYear = newChristinaSalary * 12;
+        float mashaProfit = mashaNewYear - mashaOldYear;
         System.out.println("Маша теперь получает " + newMashaSalary + " рублей. Годовой доход вырос на " + MashaProfit + " рублей");
-        float DenisProfit = DenisNewYear - DenisOldYear;
+        float DenisProfit = denisNewYear - denisOldYear;
         System.out.println("Денис теперь получает " + newDenisSalary + " рублей. Годовой доход вырос на " + DenisProfit + " рублей");
-        float ChristinaProfit = ChristinaNewYear - ChristinaOldYear;
-        System.out.println("Кристина теперь получает " + newChristinaSalary + " рублей. Годовой доход вырос на " + ChristinaProfit + " рублей");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        float christinaProfit = christinaNewYear - christinaOldYear;
+        System.out.println("Кристина теперь получает " + newChristinaSalary + " рублей. Годовой доход вырос на " + christinaProfit + " рублей");
 
 
     }
